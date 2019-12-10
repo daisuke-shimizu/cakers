24f92fdadce80be7d824570eb30b418135414af0
daisuke-shimizu <vitamin.daisuke.0901@gmail.com>
2019-12-10 14:12:43 +0000

    admin desgined first

--- app/controllers/admins/genres_controller.rb
+++ app/controllers/admins/genres_controller.rb
@@ -3,16 +3,19 @@ class Admins::GenresController < ApplicationController
         @genres = Genre.all
         @genre = Genre.new
     end
+
     def create
         @genre = Genre.new(genre_params)
         @genre.save
         redirect_to admins_genres_path
-
     end
+
     def update
     end
+
     private
     def genre_params
         params.require(:genre).permit(:title)
     end
+    
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+admin1 = Admin.new(:email => 'admin@admin', :password => '123456')
+admin1.save
+
