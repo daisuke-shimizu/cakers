2edd2df02022768c5ef2dfd3d941a2f7e5b6612a
daisuke-shimizu <vitamin.daisuke.0901@gmail.com>
2019-12-13 05:58:14 +0000

    made cart items

--- app/controllers/customers/cart_items_controller.rb
+++ app/controllers/customers/cart_items_controller.rb
@@ -1,2 +1,15 @@
 class Customers::CartItemsController < ApplicationController
+    def index
+        @cart_items = CartItem.all
+    end
+    def create
+        @cart_item = CartItem.new(cart_item_params)
+        @cart_item.customer_id = current_user.:id
+        @cart_item.save
+        redirect_to customer_cart_items_path
+    end
+    private
+        def
+            params.require(:cart_item)permit.(:amount)
+        end
 end
