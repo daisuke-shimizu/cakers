40919028d1ea74e0fbfec182a7dbe03cb8311202
daisuke-shimizu <vitamin.daisuke.0901@gmail.com>
2019-12-23 04:56:29 +0000

    first stage of order

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,6 @@
 class Order < ApplicationRecord
     belongs_to :customer
     has_many :order_details 
+    enum payment_method: { credit: 0, deposit: 1 }
+    enum which_address: {your_address: 0, other_addresses: 1, new_address: 2}
 end
